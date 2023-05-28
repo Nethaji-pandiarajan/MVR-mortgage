3e7197a148157443fd269b12db38770714ef822c
Nethaji-pandiarajan <82449478+Nethaji-pandiarajan@users.noreply.github.com>
2023-05-28 23:10:04 +0530

    Add files via upload

---
+++ Validation/validation.js
@@ -0,0 +1,21 @@
+
+
+function homeContactValidation(){
+
+    var Name = document.getElementById('contactName').value;
+    var email = document.getElementById('contactEmail').value;
+    var subject = document.getElementById('contactSubject').value;
+    var Message = document.getElementById('contactMessage').value;
+
+    if(!Name){
+
+    }
+
+    if(!email){
+
+    }
+
+    if(subject){
+        
+    }
+}
